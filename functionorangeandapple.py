59029fd721cc9220ab06060f1ee0153220e6887d
Carlosfidelrosales <fidelcarlos302@gmail.com>
2021-11-06 20:21:09 +0800

    Program Edited and Completed

--- functionorangeandapple.py
+++ functionorangeandapple.py
@@ -1,6 +1,14 @@
 def greetings():
     print("WELCOME TO CARLITO STORE! APPLE IS 20PHP EACH WHILE ORANGE IS 25PHP EACH.")
 
+
+def OrangesandApples(_apple, _orange):
+    apple_total = 20 * _apple
+    orange_total = 25 * _orange
+    sum = apple_total + orange_total
+    return sum
+
+
 def display():
     _apple = int(input('Enter how many apples do you want to buy:\n-> '))
     _orange = int(input('Enter how many oranges do you want to buy:\n-> '))
