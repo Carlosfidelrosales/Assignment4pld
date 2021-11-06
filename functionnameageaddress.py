5ffe9e796742c54fecc794fa6d2f1f13f870d6ad
Carlosfidelrosales <fidelcarlos302@gmail.com>
2021-11-06 21:08:03 +0800

    Added little design to the program

--- functionnameageaddress.py
+++ functionnameageaddress.py
@@ -1,11 +1,11 @@
 def acquireNameAgeAddress():
-   _name = input("Please enter your full name:\n-> ")
-   _age = int(input("Please enter your age:\n-> "))
-   _address = input("Please enter your full address:\n-> ")
+   _name = input("\033[1m\033[30m\033[93mPlease enter your full name:\n-> ")
+   _age = int(input("\033[1m\033[30m\033[93mPlease enter your age:\n-> "))
+   _address = input("\033[1m\033[30m\033[93mPlease enter your full address:\n-> ")
    return _name, _age, _address
 
 def display(nameF, ageF, addressF):
-    print(f"Hi, my name is {nameF}. I am {ageF} years old and I live in {addressF}.")
+    print(f"\033[1m\033[30m\033[93mHi, my name is {nameF}. I am {ageF} years old and I live in {addressF}.")
     
 
 name, age, address = acquireNameAgeAddress()
