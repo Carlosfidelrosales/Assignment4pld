ce52ea6ce0827c6af5bda39debc97e8e9d856e5a
Carlosfidelrosales <fidelcarlos302@gmail.com>
2021-11-06 20:07:35 +0800

    Unfinished Program

---
+++ functionnameageaddress.py
@@ -0,0 +1,10 @@
+def acquireNameAgeAddress():
+   _name = input("Please enter your full name:\n-> ")
+   _age = int(input("Please enter your age:\n-> "))
+   _address = input("Please enter your full address:\n-> ")
+   return _name, _age, _address
+
+def display(nameF, ageF, addressF):
+    print(f"Hi, my name is {nameF}. I am {ageF} years old and I live in {addressF}.")
+    
+
