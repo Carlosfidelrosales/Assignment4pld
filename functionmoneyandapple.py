963fb6aab2b74b0e455eb8ad30acaa3bd82d5dad
Carlosfidelrosales <fidelcarlos302@gmail.com>
2021-11-06 21:07:17 +0800

    Added If-Else Statement

--- functionmoneyandapple.py
+++ functionmoneyandapple.py
@@ -1,20 +1,20 @@
 def greetings():
-    print("WELCOME TO CARLITO STORE! The fruit available for today is an apple.")
+    print("\033[1m\033[30m\033[93mWELCOME TO CARLITO STORE! The fruit available for today is an apple.")
 
-def appleTotal(pocketMoney, price_apple):
-    totalApples = pocketMoney // price_apple
-    return totalApples
 
-def changeTotal(pocketMoney, price_apple):
-    Applesmax = price_apple % pocketMoney
-    return Applesmax
-
-def display():
-    pocketMoney = float(input("How much money do you have in your pocket right now?:\n-> "))
-    price_apple = float(input("How much is the price for each apple?:\n-> "))
-    appleF = appleTotal(pocketMoney, price_apple)
-    changeF = changeTotal(price_apple, pocketMoney)
-    print(f'You can buy {appleF} apple(s) and your change is {changeF} pesos.')
+def change_apple(pocketMoney, price_apple):
+    if pocketMoney >=  price_apple:
+        change = pocketMoney % price_apple
+        apple_max = pocketMoney // price_apple
+        print(f"You can buy {apple_max:,.0f} apple(s) and your change is {change:,.2f} pesos.")
+    else:
+        price_apple > pocketMoney
+        insufficient_money = price_apple - pocketMoney
+        print(f"I am sorry but you don't have enough money. You must need atleast additional {insufficient_money:,.2f} pesos in order to buy an apple.")
 
 greetings()
-display()
+change_apple(pocketMoney = float(input("How much money do you have in your pocket right now?: ")), price_apple = float(input("How much is the price for each apple?: ")))
+
+
+
+
